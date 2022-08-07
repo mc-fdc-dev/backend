6f01381dfdf584ae2800a8b54fea2fc4a3657115
mc_fdc <support@mc-fdc.com>
2022-08-07 11:41:54 -1000

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from core import managers
 
 from typing import List
 
-from glob import glob
+from os import listdir
 from importlib import import_module
 
 app = FastAPI()
 app.state.backend: managers.WsManager = managers.WsManager()
 
-for router in glob("./routers/*.py"):
-    app.include_router(import_module(router).router)
+for router_path in listdir("./routers"):
+    app.include_router(import_module(f"routers.{router_path[:-3]}").router)
 
 @app.on_event("startup")
 async def _startup():

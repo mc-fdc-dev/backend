820fb15863d2e659d64a68a96b92546ec319c967
mc_fdc <support@mc-fdc.com>
2022-08-07 10:17:32 -1000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI, WebSocket
+from aiomysql import create_pool
 
 app = FastAPI()
 
+
+@app.on_event("startup")
+async def _startup():
+    app.state.pool = await create_pool(host=secret.DB_URL, port=3306, user=secret.DB_USERNAME, password=secret.DB_PASSWORD, db=secret.DB_DATABASE)
+    print("startup done")
 
 @app.get("/")
 def main():

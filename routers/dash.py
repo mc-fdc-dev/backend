4b545da116c91f3c14ac3aa045312772500e97d6
mc_fdc <support@mc-fdc.com>
2022-08-07 12:48:07 -1000

    Create dash.py

---
+++ routers/dash.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter
+from fastapi.responses import RedirectResponse
+
+from core.discord import DiscordOauth2
+
+from os import getenv
+
+
+router = APIRouter(prefix="/dashboard")
+oauth = DiscordOauth2(getenv("CLIENT_ID"), getenv("CLIENT_SECRET"))
+
+
+@router.get("/")
+def main():
+    return {"message": "Hello, World"}
+
+@router.get("/redirect")
+async def redirect(code: str):
+    token = await oauth.exchange_code(code)
+    response = RedirectResponse("https://mc-fdc.com/dashboard")
+    response.set_cookie("token", token.access, expires=token.expires_in)
+    return response

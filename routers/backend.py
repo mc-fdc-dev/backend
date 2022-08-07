1b84d2f8c6e9be9cf6affdf2b848ba5558506801
mc_fdc <support@mc-fdc.com>
2022-08-07 10:31:34 -1000

    Update backend.py

--- routers/backend.py
+++ routers/backend.py
@@ -1,6 +1,36 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, WebSocket, Request
+from orjson import loads, dumps
 
+from typing import TypedDict, Any
 
 router = ApiRouter()
 
+class WsManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
 
+    async def connect(self, websocket: WebSocket) -> None:
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, type: str, data: Any, websocket: WebSocket):
+        await websocket.send_text(dumps({"type": type, "data": data}))
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+class BackendData(TypedDict):
+    type: str
+    data: Any
+
+@router.websocket("/backend")
+async def backend(req: Request, ws: WebSocket):
+    await ws.accept()
+    while True:
+        data: BackendData = loads(await ws.receive_text())
+        if data["type"] == "hello":
+            

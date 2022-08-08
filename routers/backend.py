6c1131df42854a578edc15c4c485cddaf145ae55
mc_fdc <support@mc-fdc.com>
2022-08-07 18:06:53 -1000

    Update backend.py

--- routers/backend.py
+++ routers/backend.py
@@ -19,9 +19,6 @@ async def backend(ws: WebSocket):
     await backend.connect(ws)
     try:
         data: BackendData = loads(await wait_for(ws.receive_text(), timeout=60))
-    except TimeoutError:
-        await backend.disconnect(message="Timeout")
-    else:
         if data["type"] == "login":
             if data["data"] == getenv("BACKEND_PASSWORD"):
                 await backend.send(type="success", data=None)
@@ -30,6 +27,8 @@ async def backend(ws: WebSocket):
                 await backend.disconnect(message="Password is invalid")
         else:
             await backend.disconnect(message="Please login first")
+    except TimeoutError:
+        await backend.disconnect(message="Timeout")
     while True:
         data: BackendData = loads(await ws.receive_text())
         print(data)

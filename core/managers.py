f12f213ed57e537c0426d8633193a72c31b8f6c1
mc_fdc <support@mc-fdc.com>
2022-08-07 19:55:36 -1000

    Update managers.py

--- core/managers.py
+++ core/managers.py
@@ -1,11 +1,35 @@
 from fastapi import WebSocket
-from orjson import dumps
+from orjson import dumps, loads
+
+import asyncio
 
 from typing import Any
 
 
 class WsManagerError(Exception):
     pass
+
+class HeartBeat:
+    closed: bool = False
+
+    def __init__(self, manager):
+        self.manager = manager
+
+    def run(self):
+        self.task = asyncio.create_task(self.main())
+    
+    def close(self):
+        self.task.cancel()
+
+    async def main(self):
+        while True:
+            await manager.send("heartbeat", "ping")
+            try:
+                await asyncio.wait_for(manager.recv(), timeout=10)
+            except asyncio.TimeoutError:
+                await manager.close(message="Please send heartbeat")
+                self.closed = True
+            await asyncio.sleep(60)
 
 class WsManager:
     def __init__(self):
@@ -16,6 +40,7 @@ class WsManager:
         self.active_connection = websocket
 
     async def disconnect(self):
+        self.heartbeat.close()
         try:
             await self.active_connection.close()
         except Exception:
@@ -24,3 +49,10 @@ class WsManager:
 
     async def send(self, type: str, data: Any, **kwargs):
         await self.active_connection.send_text(dumps({"type": type, "data": data}), **kwargs)
+
+    async def recv(self) -> dict:
+        return loads(await self.active_connection.receive_text())
+    
+    @property
+    def closed(self):
+        return self.heartbeat.closed
